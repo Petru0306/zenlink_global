f64b245bafecf7321cfde75153393ed2c2643a1c
Panait Mihai-Petru <petru.panait77@gmail.com>
2025-11-02 15:56:07 +0200

    commit2

---
+++ src/main/java/com/zenlink/zenlink/ZenlinkApplication.java
@@ -0,0 +1,13 @@
+package com.zenlink.zenlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zenlink/zenlink/ZenlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenlink.zenlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
